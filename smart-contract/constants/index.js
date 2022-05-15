b9887721744c46f83b6424cf98bed0c2019efb8a
Raj Patil <rjpatil53@gmail.com>
2022-05-15 17:09:18 +0530

    Initial commit

---
+++ smart-contract/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xd7DDb597dbB4e76865716e276506B7f7c5139aD7";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
